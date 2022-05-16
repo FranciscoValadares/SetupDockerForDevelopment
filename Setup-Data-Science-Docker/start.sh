312c8c0fcf40e009d1102cf47e42d332a532dead
Francisco Valadares <valadares.francisco@gmail.com>
2022-05-16 09:55:20 -0300

    Create start.sh

---
+++ Setup-Data-Science-Docker/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Cron process
+#cron -f 2>&1 &
+
+# Set Jupyter password
+sed -ie "s/#c.NotebookApp.password = u''/c.NotebookApp.password = u'$JUPYTER_PASSWORD'/g" ~/.jupyter/jupyter_notebook_config.py
+
+exec /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root
